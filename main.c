07d6a0c0e530eb83744ce0df7b8bb5f36193544c
Jose Del Valle <jpinad2100tmp@alumnoguinda.mx>
2023-06-23 11:39:40 -0600

    yola

--- main.c
+++ main.c
@@ -489,7 +489,7 @@ void Inicio(){
     printf("\t\t\t                            ####                                                                    #####\n");
 
 
-    printf("\t\t\t\t\t                                     ###\n");
+    printf("\t\t\t\t\t                                      ###\n");
     printf("\t\t\t\t\t                                      ##\n");
     printf("\t\t\t\t\t ####     #####    ####     ####      ##      ####    ######    ####     #####\n");
     printf("\t\t\t\t\t##  ##   ##       ##  ##       ##     ##     ##  ##    ##  ##      ##   ##\n");
